d64900aa567b09cc1e26c40581cc3849d643d203
izharraazi <izharraazi@gmail.com>
2015-02-11 20:05:50 -0800

    Lab 1 Hello Scala

---
+++ src/main/scala/hello/HelloConfig.scala
@@ -0,0 +1,22 @@
+package hello
+
+import org.springframework.context.annotation.{Configuration, ComponentScan}
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.web.bind.annotation._
+
+/**
+ * This config class will trigger Spring @annotation scanning and auto configure Spring context.
+ *
+ * @author Izhar Raazi
+ * @since 1.0
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@RestController
+class HelloConfig 
+{
+@RequestMapping (value = Array("/"), method = Array (RequestMethod.GET))
+@ResponseBody
+def home(): String = "Hello Scala!!"
+  }
